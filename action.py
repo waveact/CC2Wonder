18d9d208271bc0989e9c6ea2dc9908f0fc158061
waveact <waveact@gmail.com>
2023-05-22 14:37:42 +0800

    supports std and arkit expression

--- action.py
+++ action.py
@@ -1,5 +1,4 @@
-import bpy
-import os
+import bpy, os, json
 
 class CC2WonderAction(bpy.types.Operator):
     bl_idname = "object.cc2wonder_action"
@@ -10,7 +9,15 @@ class CC2WonderAction(bpy.types.Operator):
     def execute(self, context):
         addon_path = os.path.dirname(os.path.realpath(__file__))
         resource_path = os.path.join(addon_path, "resource")
+        expression_data_path = os.path.join(resource_path, "expression.json")
+        bone_data_path = os.path.join(resource_path, "bone.json")
 
+        with open(expression_data_path,'r') as f: 
+            self.expression_data = json.load(f) 
+
+        with open(bone_data_path,'r') as f: 
+            self.bone_data = json.load(f) 
+        
         armature_name = "Armature"
         armature = bpy.data.objects.get(armature_name)
         armature.name = "CC_BODY"
@@ -23,10 +30,6 @@ class CC2WonderAction(bpy.types.Operator):
         
         filepath = bpy.path.abspath(self.filepath)
         self.folder_path = os.path.dirname(filepath)
-        
-        self.convert_bone_data = {"CC_Base_L_Clavicle":"LeftShoulder","CC_Base_L_Upperarm":"LeftArm","CC_Base_L_Forearm":"LeftForeArm","CC_Base_L_Hand":"LeftHand","CC_Base_L_Thumb1":"LeftHandThumb1","CC_Base_L_Thumb2":"LeftHandThumb2","CC_Base_L_Thumb3":"LeftHandThumb3","CC_Base_L_Index1":"LeftHandIndex1","CC_Base_L_Index2":"LeftHandIndex2","CC_Base_L_Index3":"LeftHandIndex3","CC_Base_L_Mid1":"LeftHandMiddle1","CC_Base_L_Mid2":"LeftHandMiddle2","CC_Base_L_Mid3":"LeftHandMiddle3","CC_Base_L_Ring1":"LeftHandRing1","CC_Base_L_Ring2":"LeftHandRing2","CC_Base_L_Ring3":"LeftHandRing3","CC_Base_L_Pinky1":"LeftHandPinky1","CC_Base_L_Pinky2":"LeftHandPinky2","CC_Base_L_Pinky3":"LeftHandPinky3","CC_Base_L_Thigh":"LeftUpLeg","CC_Base_L_Calf":"LeftLeg","CC_Base_L_Foot":"LeftFoot","CC_Base_L_ToeBase":"LeftToeBase","CC_Base_R_Clavicle":"RightShoulder","CC_Base_R_Upperarm":"RightArm","CC_Base_R_Forearm":"RightForeArm","CC_Base_R_Hand":"RightHand","CC_Base_R_Thumb1":"RightHandThumb1","CC_Base_R_Thumb2":"RightHandThumb2","CC_Base_R_Thumb3":"RightHandThumb3","CC_Base_R_Index1":"RightHandIndex1","CC_Base_R_Index2":"RightHandIndex2","CC_Base_R_Index3":"RightHandIndex3","CC_Base_R_Mid1":"RightHandMiddle1","CC_Base_R_Mid2":"RightHandMiddle2","CC_Base_R_Mid3":"RightHandMiddle3","CC_Base_R_Ring1":"RightHandRing1","CC_Base_R_Ring2":"RightHandRing2","CC_Base_R_Ring3":"RightHandRing3","CC_Base_R_Pinky1":"RightHandPinky1","CC_Base_R_Pinky2":"RightHandPinky2","CC_Base_R_Pinky3":"RightHandPinky3","CC_Base_R_Thigh":"RightUpLeg","CC_Base_R_Calf":"RightLeg","CC_Base_R_Foot":"RightFoot","CC_Base_R_ToeBase":"RightToeBase","CC_Base_Head":"Head","CC_Base_NeckTwist01":"Neck","CC_Base_Spine02":"Spine2","CC_Base_Spine01":"Spine1","CC_Base_Waist":"Spine","CC_Base_Hip":"Hips","RL_BoneRoot":"root"}
-
-        self.convert_data = {"browInnerDnL":{"Extend":"Brow_Drop_L","Standard":"Brow_Drop_L"},"browInnerDnR":{"Extend":"Brow_Drop_R","Standard":"Brow_Drop_R"},"browInnerUpL":{"Extend":"Brow_Raise_Inner_L","Standard":"Brow_Raise_Inner_L"},"browInnerUpR":{"Extend":"Brow_Raise_Inner_R","Standard":"Brow_Raise_Inner_R"},"browOuterDnL":{"Extend":"","Standard":""},"browOuterDnR":{"Extend":"","Standard":""},"browOuterUpL":{"Extend":"Brow_Raise_Outer_L","Standard":"Brow_Raise_Outer_L"},"browOuterUpR":{"Extend":"Brow_Raise_Outer_R","Standard":"Brow_Raise_Outer_R"},"browSqueezeL":{"Extend":"Brow_Compress_L","Standard":""},"browSqueezeR":{"Extend":"Brow_Compress_R","Standard":""},"cheekBlowL":{"Extend":"Cheek_Puff_L","Standard":"Cheek_Puff_L"},"cheekBlowR":{"Extend":"Cheek_Puff_R","Standard":"Cheek_Puff_R"},"cheekUpL":{"Extend":"Cheek_Raise_L","Standard":"Cheek_Raise_L"},"cheekUpR":{"Extend":"Cheek_Raise_R","Standard":"Cheek_Raise_R"},"eyeBlinkL":{"Extend":"Eye_Blink_L","Standard":"Eye_Blink_L"},"eyeBlinkR":{"Extend":"Eye_Blink_R","Standard":"Eye_Blink_R"},"eyeCompressL":{"Extend":"Cheek_Raise_L","Standard":"Cheek_Raise_L"},"eyeCompressR":{"Extend":"Cheek_Raise_R","Standard":"Cheek_Raise_R"},"eyeDn":{"Extend":"Eye_Look_Down","Standard":"Eye_Look_Down"},"eyeL":{"Extend":"Eye_Look_L","Standard":"Eye_Look_L"},"eyeR":{"Extend":"Eye_Look_R","Standard":"Eye_Look_R"},"eyeSquintL":{"Extend":"Eye_Squint_L","Standard":"Eye_Squint_L"},"eyeSquintR":{"Extend":"Eye_Squint_R","Standard":"Eye_Squint_R"},"eyeUp":{"Extend":"Eye_Look_Up","Standard":"Eye_Look_Up"},"eyeWidenLowerL":{"Extend":"","Standard":""},"eyeWidenLowerR":{"Extend":"","Standard":""},"eyeWidenUpperL":{"Extend":"Eye_Wide_L","Standard":"Eye_Wide_L"},"eyeWidenUpperR":{"Extend":"Eye_Wide_R","Standard":"Eye_Wide_R"},"jawClenchL":{"Extend":"","Standard":""},"jawClenchR":{"Extend":"","Standard":""},"jawIn":{"Extend":"Jaw_Backward","Standard":""},"jawL":{"Extend":"Jaw_L","Standard":"Jaw_L"},"jawOpen":{"Extend":"Jaw_Open","Standard":"Jaw_Open"},"jawOut":{"Extend":"Jaw_Forward","Standard":"Jaw_Forward"},"jawR":{"Extend":"Jaw_R","Standard":"Jaw_R"},"lipChinRaiserL":{"Extend":"","Standard":""},"lipChinRaiserR":{"Extend":"","Standard":""},"lipCloseLower":{"Extend":"","Standard":""},"lipCloseUpper":{"Extend":"","Standard":""},"lipCornerDnL":{"Extend":"Mouth_Frown_L","Standard":"Mouth_Frown_L"},"lipCornerDnR":{"Extend":"Mouth_Frown_R","Standard":"Mouth_Frown_R"},"lipCornerUpL":{"Extend":"Mouth_Smile_Sharp_L","Standard":""},"lipCornerUpR":{"Extend":"Mouth_Smile_Sharp_R","Standard":""},"lipDimplerL":{"Extend":"Mouth_Dimple_L","Standard":"Mouth_Dimple_L"},"lipDimplerR":{"Extend":"Mouth_Dimple_R","Standard":"Mouth_Dimple_R"},"lipFunnelerLower":{"Extend":"","Standard":""},"lipFunnelerUpper":{"Extend":"","Standard":""},"lipLowerDnL":{"Extend":"Mouth_Roll_Out_Lower_L","Standard":""},"lipLowerDnR":{"Extend":"Mouth_Roll_Out_Lower_R","Standard":""},"lipLowerPullDnL":{"Extend":"Mouth_Pull_Lower_L","Standard":""},"lipLowerPullDnR":{"Extend":"Mouth_Pull_Lower_R","Standard":""},"lipLowerUpL":{"Extend":"Mouth_Roll_In_Lower_L","Standard":""},"lipLowerUpR":{"Extend":"Mouth_Roll_In_Lower_R","Standard":""},"lipNarrowL":{"Extend":"","Standard":""},"lipNarrowR":{"Extend":"","Standard":""},"lipPoutLower":{"Extend":"","Standard":""},"lipPoutUpper":{"Extend":"","Standard":""},"lipPresserL":{"Extend":"Mouth_Press_L","Standard":"Mouth_Press_L"},"lipPresserR":{"Extend":"Mouth_Press_R","Standard":"Mouth_Press_R"},"lipPucker":{"Extend":"","Standard":""},"lipPullL":{"Extend":"Mouth_Lower_L","Standard":"Mouth_Lower_L"},"lipPullR":{"Extend":"Mouth_Lower_R","Standard":"Mouth_Lower_R"},"lipPushLower":{"Extend":"","Standard":""},"lipPushUpper":{"Extend":"","Standard":""},"lipSmileClosedL":{"Extend":"Mouth_Smile_L","Standard":""},"lipSmileClosedR":{"Extend":"Mouth_Smile_R","Standard":""},"lipSmileOpenL":{"Extend":"","Standard":""},"lipSmileOpenR":{"Extend":"","Standard":""},"lipSneerL":{"Extend":"","Standard":""},"lipSneerR":{"Extend":"","Standard":""},"lipStickyL":{"Extend":"","Standard":""},"lipStickyR":{"Extend":"","Standard":""},"lipSuckLower":{"Extend":"","Standard":""},"lipSuckUpper":{"Extend":"","Standard":""},"lipSwingL":{"Extend":"Mouth_L","Standard":"Mouth_L"},"lipSwingR":{"Extend":"Mouth_R","Standard":"Mouth_R"},"lipTightnerL":{"Extend":"Mouth_Tighten_L","Standard":""},"lipTightnerR":{"Extend":"Mouth_Tighten_R","Standard":""},"lipUpperDnL":{"Extend":"","Standard":""},"lipUpperDnR":{"Extend":"","Standard":""},"lipUpperUpL":{"Extend":"Mouth_Up_Upper_L","Standard":"Mouth_Up_Upper_L"},"lipUpperUpR":{"Extend":"Mouth_Up_Upper_R","Standard":"Mouth_Up_Upper_R"},"lipWidenL":{"Extend":"","Standard":""},"lipWidenR":{"Extend":"","Standard":""},"noseCompress":{"Extend":"","Standard":""},"noseFlare":{"Extend":"","Standard":""},"noseSneerL":{"Extend":"Nose_Sneer_L","Standard":"Nose_Sneer_L"},"noseSneerR":{"Extend":"Nose_Sneer_R","Standard":"Nose_Sneer_R"},"noseWrinklerL":{"Extend":"Nose_Crease_L","Standard":""},"noseWrinklerR":{"Extend":"Nose_Crease_R","Standard":""}}
    
         self.shape_key_mesh = None
         
@@ -51,12 +54,21 @@ class CC2WonderAction(bpy.types.Operator):
         self.change_bone_parent()
         
         self.rename_mesh()
+        
         self.build_material()
         
         self.merge_shape_keys("Eye_L_Look_L", "Eye_R_Look_L", "eyeL")
+        self.merge_shape_keys("A10_Eye_Look_Out_Left", "A12_Eye_Look_In_Right", "eyeL")
+        
         self.merge_shape_keys("Eye_L_Look_R", "Eye_R_Look_R", "eyeR")
+        self.merge_shape_keys("A11_Eye_Look_In_Left", "A13_Eye_Look_Out_Right", "eyeR")
+        
         self.merge_shape_keys("Eye_L_Look_Up", "Eye_R_Look_Up", "eyeUp")
+        self.merge_shape_keys("A06_Eye_Look_Up_Left", "A07_Eye_Look_Up_Right", "eyeUp")
+        
         self.merge_shape_keys("Eye_L_Look_Down", "Eye_R_Look_Down", "eyeDn")
+        self.merge_shape_keys("A08_Eye_Look_Down_Left", "A09_Eye_Look_Down_Right", "eyeDn")
+        
         self.merge_shape_keys("Mouth_Roll_Out_Lower_L", "Mouth_Roll_Out_Lower_R", "lipPoutLower")
         self.merge_shape_keys("Mouth_Roll_Out_Upper_L", "Mouth_Roll_Out_Upper_R", "lipPoutUpper")
         self.merge_shape_keys("Mouth_Push_Lower_L", "Mouth_Push_Lower_R", "lipPushLower")
@@ -102,14 +114,14 @@ class CC2WonderAction(bpy.types.Operator):
     def rename_bones(self):
         bpy.ops.object.mode_set(mode='POSE')
        
-        for name in self.convert_bone_data:
+        for name in self.bone_data:
             # get the pose bone with name
             bone = self.armature.pose.bones.get(name)
             # continue if no bone of that name
             if bone is None:
                 continue
             # rename
-            bone.name = self.convert_bone_data[name]
+            bone.name = self.bone_data[name]
             
         bpy.ops.object.mode_set(mode='OBJECT')
     
@@ -124,6 +136,12 @@ class CC2WonderAction(bpy.types.Operator):
                 self.mesh_obj_list[i].name = self.mesh_name + "_Base_FACE"
                 self.mesh_obj_list[i].data.name = self.mesh_name + "_Base"
                 self.shape_key_mesh = self.mesh_obj_list[i]
+                
+            if self.mesh_obj_list[i].name == "CC_Game_Body":
+                self.mesh_obj_list[i].name = self.mesh_name + "_Base_FACE"
+                self.mesh_obj_list[i].data.name = self.mesh_name + "_Base"
+                self.shape_key_mesh = self.mesh_obj_list[i]
+            
     
     def build_material(self):
         folder_path = self.folder_path
@@ -164,8 +182,12 @@ class CC2WonderAction(bpy.types.Operator):
                 for node in nodes:
                     if node.type == "BSDF_PRINCIPLED":
                         bsdf = node
-                        diffuse_channel = bsdf.inputs["Base Color"].links[0].from_node
-                        normal_channel = bsdf.inputs["Normal"].links[0].from_node
+                        if len(bsdf.inputs["Base Color"].links) > 0:
+                            diffuse_channel = bsdf.inputs["Base Color"].links[0].from_node
+                            
+                        if len(bsdf.inputs["Normal"].links) > 0:
+                            normal_channel = bsdf.inputs["Normal"].links[0].from_node
+                            
                         if normal_channel.type == 'NORMAL_MAP':
                             if len(normal_channel.inputs["Color"].links) > 0 :
                                 normal_channel = bsdf.inputs["Normal"].links[0].from_node.inputs["Color"].links[0].from_node
@@ -265,13 +287,18 @@ class CC2WonderAction(bpy.types.Operator):
     def rename_shape_keys(self):
         mesh_obj = self.shape_key_mesh
         shape_keys = mesh_obj.data.shape_keys.key_blocks
-        for k in self.convert_data:
-            for index, key in enumerate(shape_keys):
-                if key.name == self.convert_data[k]["Extend"]:
-                    key.name = k
-                if key.name == self.convert_data[k]["Standard"]:
-                    key.name = k
-                key.value = 0.0
+        if shape_keys is not None:
+            for k in self.expression_data:
+                for index, key in enumerate(shape_keys):
+                    if key.name == self.expression_data[k]["Extend"]:
+                        key.name = k
+                    if key.name == self.expression_data[k]["Standard"]:
+                        key.name = k
+                    if key.name == self.expression_data[k]["ARKit"]:
+                        key.name = k
+                    if key.name == self.expression_data[k]["Tradition"]:
+                        key.name = k
+                    key.value = 0.0
     
     def reset_shape_keys(self):
         bpy.ops.object.mode_set(mode='OBJECT')
@@ -323,7 +350,7 @@ class CC2WonderAction(bpy.types.Operator):
             _MC_shape    = "Mouth_Close"
             
             if shape_keys is not None:
-                if _MFUpL_shape in shape_keys.key_blocks and _MFUpR_shape in shape_keys.key_blocks:
+                if _MFUpL_shape in shape_keys.key_blocks and _MFUpR_shape in shape_keys.key_blocks :
                     MFUpL_key = shape_keys.key_blocks[_MFUpL_shape]
                     MFUpR_key = shape_keys.key_blocks[_MFUpR_shape]
                     MFDnL_key = shape_keys.key_blocks[_MFDnL_shape]
